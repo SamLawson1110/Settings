ae15b2333db7a53839f859a204253d4ac6bd452f
Sam <103685461+SamLawson1110@users.noreply.github.com>
2022-07-27 14:08:21 -0400

    added settings.js

---
+++ settings/src/components/Settings.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+function Settings() {
+    return (
+        <Button variant="outlined">Outlined</Button>
+    );
+  }
+  
+  export default Settings;  
